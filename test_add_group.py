274c04da3db53941a37f5b5498bbc291f501dcec
a.safonova <safonova_alla@mail.ru>
2022-05-02 21:19:42 +0300

    задание №1

--- test_add_group.py
+++ test_add_group.py
@@ -25,25 +25,29 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.NAME, "new").click()
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys("boboo")
+        wd.find_element(By.NAME, "group_name").send_keys("boo")
         wd.find_element(By.NAME, "group_header").click()
         wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys("booboo")
+        wd.find_element(By.NAME, "group_header").send_keys("boo")
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys("booboo")
+        wd.find_element(By.NAME, "group_footer").send_keys("boo")
         wd.find_element(By.NAME, "submit").click()
         wd.find_element(By.LINK_TEXT, "groups").click()
         wd.find_element(By.LINK_TEXT, "Logout").click()
     
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
     
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
     
     def tearDown(self):
